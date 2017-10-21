89da6dd4b7755a9844da5df716598d9b71c05c53
wiserubi <wiserubi@gmail.com>
2017-10-21 16:26:33 +0900

    로그인통신 및 처리 부분을 추가

--- app.js
+++ app.js
@@ -1,46 +1,276 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
-
+var bodyParser = require('body-Parser');
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(__dirname+'/public'));
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+var cors = require('cors')();
+app.use(cors);
 
-app.use('/', index);
-app.use('/users', users);
+var users =[];
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'test1234',
+  database : 'realestat'
+});
+ 
+connection.connect();
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+var MongoClient = require('mongodb').MongoClient;
+ var url = 'mongodb://localhost:27017/restful';
+ var dbObj = null;
+ MongoClient.connect(url, function(err, db) {
+   console.log("Connected correctly to server");
+   dbObj = db;
+ });
+
+var multer = require('multer');
+var Storage = multer.diskStorage({
+     destination: function(req, file, callback) {
+         callback(null, "./public/upload_image/");
+     },
+     filename: function(req, file, callback) {
+     		file.uploadedFile = file.fieldname + "_" + 
+     			Date.now() + "_" + file.originalname;
+     		console.log('file.uploadedFile:'+file.uploadedFile);
+         callback(null, file.uploadedFile);
+     }
+ });
+ var upload = multer({
+     storage: Storage
+ }).single("image");
+app.post('/user/picture',function(req, res) {
+	upload(req, res, function(err) {
+		if (err) {
+			res.send(JSON.stringify(err));
+		} else {
+			res.send(JSON.stringify({url:req.file.uploadedFile,
+				description:req.body.description}));
+		}
+	});
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.get('/user/message',function(req,res){
+	console.log(req.query.sender_id);
+	var condition = {};
+	if(req.query.sender_id != undefined)
+		condition = {sender_id:req.query.sender_id};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	var messages = dbObj.collection('messages');
+
+	messages.find(condition)
+	.toArray(function(err,results){
+		if (err) {
+ 		    res.send(JSON.stringify(err));
+ 		} else {
+ 			res.send(JSON.stringify(results));
+ 		}
+
+	});
+
+
+});
+var ObjectID = require('mongodb').ObjectID;
+
+app.get('/user/message/:id',function(req,res){
+	var messages = dbObj.collection('messages');
+	messages.findOne(
+ 		{_id:ObjectID.createFromHexString(req.params.id)},
+ 		function(err, result){
+ 			if (err) {
+ 				res.send(JSON.stringify(err));
+ 			} else {
+ 				res.send(JSON.stringify(result));
+ 			}
+ 		});
+  });
+
+app.post('/user/message',function(req,res) {
+ 	console.log(req.body.sender_id);
+ 	console.log(req.body.reciever_id);
+ 	console.log(req.body.message);
+ 	connection.query(
+		'select id,name from user where id=? or id=?',
+ 		[req.body.sender_id,req.body.reciever_id],
+ 		function(err, results, fields) {
+ 			if (err) {
+ 				res.send(JSON.stringify(err));
+ 			} else {
+ 				var sender = {};
+ 				var reciever = {};
+ 				for (var i = 0; i < results.length; i++){
+ 					if (results[i].id == 
+ 						Number(req.body.sender_id)) {
+ 						sender = results[i];
+ 					}
+ 					if (results[i].id ==
+ 						Number(req.body.reciever_id)) {
+ 						reciever = results[i];
+ 					}
+ 				}
+ 				var object = {
+ 					sender_id:req.body.sender_id,
+ 					reciever_id:req.body.reciever_id,
+ 					sender:sender, reciever:reciever,
+ 					message:req.body.message,
+ 					created_at:new Date()
+ 				}
+ 				var messages = dbObj.collection('messages');
+ 				messages.save(object, function(err, result){
+ 					if (err) {
+ 						res.send(JSON.stringify(err));
+ 					} else {
+ 						res.send(JSON.stringify(result));
+ 					}
+ 				});
+ 			}
+ 		});
+ });
+
+app.delete('/user/message/:id',function(req,res) {
+ 
+ 	var messages = dbObj.collection('messages');
+ 	messages.remove(
+ 		{_id:ObjectID.createFromHexString(req.params.id)},
+ 		function(err, result){
+ 			if (err) {
+ 				res.send(JSON.stringify(err));
+ 			} else {
+				res.send(JSON.stringify(result));
+ 			}
+ 		});
+  });
+  
+
+
+app.get('/user',function(req ,res){
+	//res.send(JSON.stringify(users));
+	connection.query('select * from users',
+		function(err,result,fields){
+			if(err){
+				res.send(JSON.stringify(err));
+			} else{
+				res.send(JSON.stringify(result));
+			}
+		});
 });
 
-module.exports = app;
+app.get('/offer',function(req ,res){
+	//res.send(JSON.stringify(loan));
+	connection.query('select * from offer',
+		function(err,result,fields){
+			if(err){
+				res.send(JSON.stringify(err));
+			} else{
+				res.send(JSON.stringify(result));
+			}
+		});
+});
+
+app.get('/loan',function(req ,res){
+	//res.send(JSON.stringify(loan));
+	connection.query('select * from loan',
+		function(err,result,fields){
+			if(err){
+				res.send(JSON.stringify(err));
+			} else{
+				res.send(JSON.stringify(result));
+			}
+		});
+});
+
+
+app.get('/user/:id',function(req,res){
+	connection.query('select * from users where id=?',
+		[req.params.id],function(err,results,fields){
+			if(err){
+				res.send(JSON.stringify(err));
+			} else{
+				if(results.length > 0){
+					res.send(JSON.stringify(results[0]));
+
+				}else{
+					res.send(JSON.stringfy({}));
+				}
+			
+			}
+
+		});
+
+});
+app.post('/user',function(req,res){ 
+	connection.query(
+ 		'insert into users(id,password,name,email,phone,address) values(?,? ,? ,? ,?,?)',
+ 		[ req.body.id, req.body.password , req.body.name, 
+ 		  req.body.email, req.body.phone , req.body.address], 
+ 		function(err, result) {
+ 			if (err) {
+ 				res.send(JSON.stringify(err));
+ 			} else {
+ 				res.send(JSON.stringify(result));
+ 			}
+ 		})
+  });
+
+app.post('/offer',function(req,res){ 
+	connection.query(
+ 		'insert into offer(id,date,location_code,location_dong,location_dcode, total_amt,'+
+ 		 'original_amt, premium, rent , migration_fee, tax , jisang, myinterest '+
+ 		 'values(?,? ,? ,? ,?,? ,?,? ,? ,? ,?,? ,?,?)',
+ 		[ req.body.id, req.body.date , req.body.location, 
+ 		  req.body.location_code, req.body.location_dong , req.body.location_dcode,
+ 		  req.body.total_amt, req.body.original_amt , req.body.premium,
+ 		  req.body.total_amt, req.body.original_amt , req.body.premium,
+ 		  req.body.rent, req.body.migration_fee , req.body.tax , req.body.jisang , req.body.myinterest  ], 
+ 		function(err, result) {
+ 			if (err) {
+ 				res.send(JSON.stringify(err));
+ 			} else {
+ 				res.send(JSON.stringify(result));
+ 			}
+ 		})
+  });
+
+
+app.put('/user/:id',function(req,res){
+	connection.query(
+		'update users set name=?,age=? where id=?',
+		[req.body.name, req.body.age, req.params.id],
+		function(err,result){
+			if(err){
+				res.send(JSON.stringify(err));
+			} else{
+				res.send(JSON.stringify(result));
+			}
+		});
+
+});
+app.delete('/user/:id',function(req,res){
+ 
+ 	connection.query('delete from users where id=?',
+ 		[ req.params.id ], function(err, result) {
+ 			if (err) {
+ 				res.send(JSON.stringify(err));
+ 			} else {
+ 				res.send(JSON.stringify(result));
+ 			}
+ 		});
+  });
+
+app.delete('/offer/:id',function(req,res){
+ 
+ 	connection.query('delete from offer where id=?',
+ 		[ req.params.id ], function(err, result) {
+ 			if (err) {
+ 				res.send(JSON.stringify(err));
+ 			} else {
+ 				res.send(JSON.stringify(result));
+ 			}
+ 		});
+  });
+app.listen(52273,function(){
+	console.log('Sever running');
+});
--- routes/index.js
+++ routes/index.js
@@ -6,23 +6,26 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'RealEstate' });
 });
 
-
+//회원로그인
 router.post('/usr/login', function(req,res){
 	var id = req.body.id;
 	var password = req.body.password;
 	res.send(JSON.stringify({id:id,password:password}));
 });
 
+//회원가입
 router.post('/usr', function(req,res){
 	var id = req.body.id;
 	var password= req.body.password;
 	res.send(JSON.stringify({id:id,password:password}));
 });
 
+//회원조회
 router.get('/usr', function(req,res){
 	var rowid = req.query.rowid;
 	res.send(JSON.stringify({rowid:rowid}));
 });
+//회원수정
 
 router.put('/usr', function(req,res){
 	var rowid = req.body.rowid;
@@ -32,16 +35,18 @@ router.put('/usr', function(req,res){
 	res.send(JSON.stringify({rowid:rowid,id:id,password:password}));	
 });
 
+//회원탈퇴
 router.delete('/usr', function(req,res){
 	var rowid = req.body.rowid;
 	res.send(JSON.stringify({}));
 });
 
+//전체회원조회
 router.get('/usr/list', function(req,res){
 	res.send(JSON.stringify({}));
 });
 
-
+//매물등록
 router.post('/offer', function(req,res){
 	var date = req.body.date;
 	var location = req.body.location;
@@ -55,31 +60,37 @@ router.post('/offer', function(req,res){
 	var gisang = req.body.gisang;
 	var myinterest = req.body.myinterest;
 
-	res.send(JSON.stringify({date:date,location:location,total_amt:total_amt,original_amt:original_amt,
-	     premium:premium,rent:rent,loan:loan,migraiont_fee:migration_fee,tax:tax,gisang:gisang,interest:interest}));
+	res.send(JSON.stringify({date:date,location:location,total_amt:total_amt,
+		original_amt:original_amt,premium:premium,rent:rent,loan:loan,
+		migraiont_fee:migration_fee,tax:tax,gisang:gisang,interest:interest}));
 });
 
+//매물삭제
 router.delete('/offer', function(req,res){
 	var rowid = req.body.rowid;
 	res.send(JSON.stringify({}));
 });
 
+//전체매물조회
 router.get('/offer/list', function(req,res){
 	res.send(JSON.stringify({}));
 });
 
+// 베스트매물조회
 router.get('/offer/best', function(req,res){
 	var totoamt = req.query.totamt;
 	res.send(JSON.stringify({totmat:totamt}));
 
 });
 
+// 지역별 매물조회
 router.get('/offer/location', function(req,res){
 	var location = req.query.location;
 	res.send(JSON.stringify({location:location}));
 
 });
 
+// 내관심 매물 조회
 router.get('/offer/interest', function(req,res){
 	var interest = req.query.interest;
 	res.send(JSON.stringify({interest:interest}));
